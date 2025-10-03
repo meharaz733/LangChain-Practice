97a5c7922bc4945db3709daeafaa808c236fe3be
meharaz733 <meharaz733@gmail.com>
2025-10-03 06:30:43 +0600

    practice

--- Prompts/C_P_T_wihtPlaceholder.py
+++ Prompts/C_P_T_wihtPlaceholder.py
@@ -6,23 +6,24 @@ load_dotenv()
 Chat_History = []
 
 # #Create Chat Prompt Template
-# Chat_prompt = ChatPromptTemplate([
-#                                      ('system', "Let's you are a customar support agent"),
+Chat_prompt = ChatPromptTemplate([
+                                     ('system', "Let's you are a customar support agent"),
 #                                      # MessagesPlaceholder(variable_name='Chat_History'),
-#                                      ('human', '{Query}')
-#                                  ])
+                                     ('human', '{Query}')
+                                 ])
 # #load Chat History
 # # with open('Chat_History.txt') as chatFile:
 # #     Chat_History.extend(chatFile.readlines())
 
-# #print(Chat_History)
+print(Chat_History)
 
-# #Fill the Template
-# prompt = Chat_prompt.invoke({
-#                                 # 'Chat_History': Chat_History,
-#                                 'Query':"Where is my refound?"
-#                             })
-# print(prompt)
+ #Fill the Template
+prompt = Chat_prompt.invoke({
+                                'Query':"Where is my refound?"
+                            })
+print(prompt)
+#
+exit()
 
 list_of_tuple1 = [("system", "lets a you're bolod")]
 list_of_tuple2 = [("human", "hi"), ("ai", "hello, how I can assist you today?")]
